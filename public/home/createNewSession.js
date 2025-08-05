33ce0896fddee0333c1672e0f9725ed505caa2a8
Alexandru Iacob <icb.alexunder@yahoo.com>
2025-08-05 19:53:41 +0300

    1. Apply SRP; Switch controllers to components;

---
+++ public/home/createNewSession.js
@@ -0,0 +1,25 @@
+angular.module('app').controller('createNewSession', {
+  templateUrl: '/home/createNewSession.html',  bindings: {
+    userSessions: '='
+  },
+  controller: function (toastr, userSessions, currentIdentity, sessions) {
+    this.userSessions = userSessions;
+
+    this.create = function () {
+      var newUserSession = {
+        title: this.title,
+        length: parseInt(this.length),
+        abstract: this.abstract,
+        userFirstName: currentIdentity.currentUser.firstName,
+        userLastName: currentIdentity.currentUser.lastName,
+        userId: currentIdentity.currentUser.id,
+      }
+
+      sessions.createNewSession(newUserSession).then(function (response) {
+        console.log(response);
+        this.userSessions.push(response.data);
+      }.bind(this))
+
+    }
+  }
+})
--- test/userDetailsCtrlSpec.js
+++ test/userDetailsCtrlSpec.js
@@ -1,21 +1,22 @@
 
-describe('userDetailsCtrlCtrl', function() {
+describe('userDetails', function() {
   var $ctrlCnst; 
   
   beforeEach(module('app'));
   
-  beforeEach(inject(function($controller) {
-    $ctrlCnst = $controller;
+  beforeEach(inject(function($componentController) {
+    $ctrlCnst = $componentController;
   }));
   
   it('should set the user on the controller to the matching user by id', function() {
     
-    var ctrl = $ctrlCnst('userDetailsCtrl',
+    var ctrl = $ctrlCnst('userDetails',
+      {
+        '$routeParams': {id:3}
+      },
       {
         allUsers: [{id:1,name:'wrong'},{id:3,name:'correct'}], 
-        '$routeParams': {id:3}
-      })
-      
+      })   
     expect(ctrl.user.name).toBe('correct');
   })
 })
